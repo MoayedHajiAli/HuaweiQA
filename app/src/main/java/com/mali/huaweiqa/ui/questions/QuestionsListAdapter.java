9c3701cb20b0ff6a196186e7f9efde04fdf50510
Moayed Haji Ali <mali18@ku.edu.tr>
2020-12-10 02:40:24 +0300

    allow selection to questions

--- app/src/main/java/com/mali/huaweiqa/ui/questions/QuestionsListAdapter.java
+++ app/src/main/java/com/mali/huaweiqa/ui/questions/QuestionsListAdapter.java
@@ -5,37 +5,108 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
 import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.constraintlayout.widget.ConstraintLayout;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.mali.huaweiqa.R;
 import com.mali.huaweiqa.domain.questions.Question;
-import com.mali.huaweiqa.domain.questions.QuestionsCategory;
 import com.mali.huaweiqa.domain.users_profile.Student;
 
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
 
-public class QuestionsListAdapter extends ArrayAdapter<Student> {
+public class QuestionsListAdapter extends RecyclerView.Adapter<QuestionsListAdapter.QuestionListViewHolder> {
 
     private ArrayList<Question> questions;
-    public QuestionsListAdapter(Context context) {
-        super(context, 0);
+    private HashSet<Question> selectedQuestions;
+    private QuestionListener listener;
+
+    public  QuestionsListAdapter(QuestionListener listener){
+        this.listener = listener;
+        this.questions = new ArrayList<>();
+        this.selectedQuestions = new HashSet<>();
+    }
+
+    @NonNull
+    @Override
+    public QuestionListViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return new QuestionListViewHolder(
+                LayoutInflater.from(parent.getContext()).inflate(
+                        R.layout.fragment_question_view,
+                        parent,
+                        false
+                )
+        );
     }
 
     @Override
-    public View getView(int position, View convertView, ViewGroup parent) {
-        LayoutInflater inflater = (LayoutInflater) getContext().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-        View rowView = inflater.inflate(R.layout.fragment_question_view, parent, false);
-        TextView nameView = rowView.findViewById(R.id.question_lst_body);
-        nameView.setText(questions.get(position).getQuestionBody());
-        return rowView;
+    public void onBindViewHolder(@NonNull QuestionListViewHolder holder, int position) {
+        holder.bindQuestion(questions.get(position));
     }
 
     @Override
-    public int getCount() {
+    public int getItemCount() {
         return questions.size();
     }
 
+
     public void setQuestions(ArrayList<Question> questions) {
         this.questions = questions;
+    }
+
+
+    class QuestionListViewHolder extends RecyclerView.ViewHolder{
+
+        ImageView isSelectedImg;
+        TextView questionBody;
+        ConstraintLayout layoutView;
+
+        public QuestionListViewHolder(@NonNull View itemView) {
+            super(itemView);
+            isSelectedImg = itemView.findViewById(R.id.isSelectedQuestion);
+            questionBody = itemView.findViewById(R.id.questionListBody);
+            layoutView = itemView.findViewById(R.id.layoutQuestionView);
+        }
+
+        void bindQuestion(final Question question){
+            questionBody.setText(question.getQuestionBody());
+            if(selectedQuestions.contains(question)){
+                isSelectedImg.setVisibility(View.VISIBLE);
+            }
+            else{
+                isSelectedImg.setVisibility(View.GONE);
+            }
+
+            layoutView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    if(selectedQuestions.contains(question)){
+                        selectedQuestions.remove(question);
+                        isSelectedImg.setVisibility(View.GONE);
+                        if(selectedQuestions.isEmpty()){
+                            //notify the UI the make quiz button non-visible
+                            listener.onMakeQuizAction(false);
+                        }
+                    }
+                    else
+                    {
+                        selectedQuestions.add(question);
+                        isSelectedImg.setVisibility(View.VISIBLE);
+
+                        //notify the UI the make quiz button non-visible
+                        listener.onMakeQuizAction(true);
+                    }
+                }
+            });
+        }
+    }
+
+    interface QuestionListener{
+        void onMakeQuizAction(Boolean isSelected);
     }
 }
--- app/src/main/java/com/mali/huaweiqa/ui/questions/QuestionsListFragment.java
+++ app/src/main/java/com/mali/huaweiqa/ui/questions/QuestionsListFragment.java
@@ -4,32 +4,36 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.AdapterView;
-import android.widget.ListView;
+import android.widget.Button;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
-import androidx.navigation.Navigation;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.mali.huaweiqa.R;
 import com.mali.huaweiqa.domain.questions.Question;
 
 import java.util.ArrayList;
 
-public class QuestionsListFragment extends Fragment {
+public class QuestionsListFragment extends Fragment implements QuestionsListAdapter.QuestionListener {
 
     private QuestionsListViewModel questionsViewModel;
+
+    private Button makeQuizButton;
+    private RecyclerView questionsList;
+    private QuestionsListAdapter adapter;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
 
         questionsViewModel =
                 new ViewModelProvider(this).get(QuestionsListViewModel.class);
-        View root = inflater.inflate(R.layout.category_main, container, false);
-        final ListView questionsList = root.findViewById(R.id.category_list);
-        final QuestionsListAdapter adapter = new QuestionsListAdapter(getActivity().getBaseContext());
+        View root = inflater.inflate(R.layout.quiz_list_main, container, false);
+        questionsList = root.findViewById(R.id.questionListView);
+        makeQuizButton = root.findViewById(R.id.makeQuiz);
+        adapter = new QuestionsListAdapter(this);
 
         // retrieve data on changes
         questionsViewModel.getQuestions(getArguments().getInt("CategoryIndex")).observe(getViewLifecycleOwner(), new Observer<ArrayList<Question>>() {
@@ -42,18 +46,33 @@ public class QuestionsListFragment extends Fragment {
         });
 
 
-        questionsList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+//        questionsList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+//
+//            @Override
+//            public void onItemClick(AdapterView<?> parent, final View view,
+//                                    int position, long id) {
+//                System.out.println("Item: " + position + "was clicked");
+//                Bundle bundle = new Bundle();
+//                Navigation.findNavController(view).navigate(R.id.nav_questions, bundle);
+//            }});
 
+        // make a quiz when clicking make quiz
+        makeQuizButton.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onItemClick(AdapterView<?> parent, final View view,
-                                    int position, long id) {
-                System.out.println("Item: " + position + "was clicked");
-                Bundle bundle = new Bundle();
-                Navigation.findNavController(view).navigate(R.id.nav_questions, bundle);
-            }});
+            public void onClick(View v) {
+
+            }
+        });
         return root;
     }
 
-
-
+    @Override
+    public void onMakeQuizAction(Boolean isSelected) {
+        if(isSelected) {
+            makeQuizButton.setVisibility(View.VISIBLE);
+        }
+        else{
+            makeQuizButton.setVisibility(View.GONE);
+        }
+    }
 }
